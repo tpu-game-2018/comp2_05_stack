68d47f9010145dc89cffa2f8b68fdbdd0339f42b
i-takabe <38421033+i-takabe@users.noreply.github.com>
2018-11-04 20:28:58 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,52 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) 
-{
-    char str[7];
-    
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
-    }
+#define STUCKCAPA 10 //スタックできるデータ数
 
-    return 0;
+int main(int argc, char *argv[])
+{
+	char str[7];
+	int data;
+	int stuck[STUCKCAPA];
+	int stuckNum = 0;
+	int i = 0;
+    
+	while (fgets(str, sizeof(str), stdin))
+	{
+		data = atoi(str);
+		switch (data)
+		{
+		    case 0:	
+			    i = stuckNum-1;
+			    while (i >= 0)
+			    {
+			    	printf("%d", stuck[i]);
+			    	i--;
+
+			    	if (i >= 0)
+			    		printf(",");
+			    	else
+			    		printf("\n");
+			    }
+			    break;
+
+		    case -1:
+		    	if (stuckNum > 0) 
+		    	{
+		    		stuckNum--;
+		    		printf("%d\n", stuck[stuckNum]);	
+		    	}
+		    	else
+                {
+		    		printf("\n");
+                }
+		    	break;
+
+	    	default:
+	    		stuck[stuckNum] = data;
+		    	stuckNum++;
+		    	break;
+		}
+	}
+
+	return 0;
 }
