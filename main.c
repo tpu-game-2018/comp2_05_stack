0fefe2c4fd8081dde32193b8d71ab29d25869ccf
oonuki1030 <43439440+oonuki1030@users.noreply.github.com>
2018-11-04 22:36:20 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
- #define STUCKCAPA 10 
+ #define STOCKCAPA 10 
 #define STACKCAPA 10
  int main(int argc, char *argv[])
 {
 	char str[7];
 	int data;
-	int stuck[STUCKCAPA];
-	int stuckNum=0;
+	int stock[STOCKCAPA];
+	int stockNum=0;
 	int stack[STACKCAPA];
 	int stackNum=0;
 	int i;
@@ -16,22 +16,22 @@
  		data = atoi(str);
 		switch (data){
 		    case 0:
-				if(stuckNum > 0)
+				if(stockNum > 0)
 				if(stackNum > 0){
-			   		i = stuckNum-1;
+			   		i = stockNum-1;
 			   		i = stackNum-1;
 			   		while (i >= 0){
-			    		printf("%d", stuck[i]);
+			    		printf("%d", stock[i]);
 			    		printf("%d", stack[i]);
 			    		i--;
  			    		if (i >= 0)
 @@ -37,10 +37,10 @@ int main(int argc, char *argv[])
 			    break;
  		    case -1:
-		    	if (stuckNum > 0) 
+		    	if (stockNum > 0) 
 		    	if (stackNum > 0) {
-		    		stuckNum--;
-		    		printf("%d\n", stuck[stuckNum]);	
+		    		stockNum--;
+		    		printf("%d\n", stock[stockNum]);	
 		    		stackNum--;
 		    		printf("%d\n", stack[stackNum]);	
 		    	}
@@ -40,9 +40,9 @@
                	}
 		    	break;
  	    	default:
-				if(stuckNum < STUCKCAPA)
+				if(stockNum < STOCKCAPA)
 				if(stackNum < STACKCAPA){
-	    			stuck[stuckNum] = data;
+	    			stock[stockNum] = data;
 		    		stuckNum++;
 	    			stack[stackNum] = data;
 		    		stackNum++;
