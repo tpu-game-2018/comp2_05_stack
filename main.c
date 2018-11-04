81abf51997e88f2c3f6ce7ffd7b79ba603960bd9
oonuki1030 <43439440+oonuki1030@users.noreply.github.com>
2018-11-04 22:28:35 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,55 @@
 #include <stdio.h>
-
-int main(int argc, char *argv[]) 
+ #define STUCKCAPA 10 
+#define STACKCAPA 10
+ int main(int argc, char *argv[])
 {
-    char str[7];
+	char str[7];
+	int data;
+	int stuck[STUCKCAPA];
+	int stuckNum=0;
+	int stack[STACKCAPA];
+	int stackNum=0;
+	int i;
     
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
-    }
-
-    return 0;
-}
+	while (fgets(str, sizeof(str), stdin))
+ 	{
+ 		data = atoi(str);
+		switch (data){
+		    case 0:
+				if(stuckNum > 0)
+				if(stackNum > 0){
+			   		i = stuckNum-1;
+			   		i = stackNum-1;
+			   		while (i >= 0){
+			    		printf("%d", stuck[i]);
+			    		printf("%d", stack[i]);
+			    		i--;
+ 			    		if (i >= 0)
+@@ -37,10 +37,10 @@ int main(int argc, char *argv[])
+			    break;
+ 		    case -1:
+		    	if (stuckNum > 0) 
+		    	if (stackNum > 0) {
+		    		stuckNum--;
+		    		printf("%d\n", stuck[stuckNum]);	
+		    		stackNum--;
+		    		printf("%d\n", stack[stackNum]);	
+		    	}
+		    	else{
+ 		    		printf("\n");
+               	}
+		    	break;
+ 	    	default:
+				if(stuckNum < STUCKCAPA)
+				if(stackNum < STACKCAPA){
+	    			stuck[stuckNum] = data;
+		    		stuckNum++;
+	    			stack[stackNum] = data;
+		    		stackNum++;
+				}
+		    	break;
+		}
+ 	}
+ 
+ 	return 0;
+ }
