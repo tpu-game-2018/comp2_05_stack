8df994a4e8850710a5712230f9cde34084d35ab8
i-takabe <38421033+i-takabe@users.noreply.github.com>
2018-11-04 21:58:01 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-#define STUCKCAPA 10 //スタックできるデータ数
+#define STACKCAPA 10 //スタックできるデータ数
 
 int main(int argc, char *argv[])
 {
 	char str[7];
 	int data;
-	int stuck[STUCKCAPA];
-	int stuckNum = 0;
+	int stack[STACKCAPA];
+	int stackNum = 0;
 	int i = 0;
     
 	while (fgets(str, sizeof(str), stdin))
@@ -16,12 +16,12 @@ int main(int argc, char *argv[])
 		switch (data)
 		{
 		    case 0:
-				if(stuckNum > 0)
+				if(stackNum > 0)
 				{
-			   		i = stuckNum-1;
+			   		i = stackNum-1;
 			   		while (i >= 0)
 			    	{
-			    		printf("%d", stuck[i]);
+			    		printf("%d", stack[i]);
 			    		i--;
 
 			    		if (i >= 0)
@@ -37,10 +37,10 @@ int main(int argc, char *argv[])
 			    break;
 
 		    case -1:
-		    	if (stuckNum > 0) 
+		    	if (stackNum > 0) 
 		    	{
-		    		stuckNum--;
-		    		printf("%d\n", stuck[stuckNum]);	
+		    		stackNum--;
+		    		printf("%d\n", stack[stackNum]);	
 		    	}
 		    	else
                	{
@@ -49,10 +49,10 @@ int main(int argc, char *argv[])
 		    	break;
 
 	    	default:
-				if(stuckNum < STUCKCAPA)
+				if(stackNum < STACKCAPA)
 				{
-	    			stuck[stuckNum] = data;
-		    		stuckNum++;
+	    			stack[stackNum] = data;
+		    		stackNum++;
 				}
 		    	break;
 		}
