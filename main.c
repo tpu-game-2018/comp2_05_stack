e0498c04faabae25bbb650e53eb91c43a4c9c03e
kireha <43433438+kireha@users.noreply.github.com>
2018-11-05 15:07:03 +0900

    Update main.c

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
             printf("\n");
         }
         else if(x==-1){
-            printf("%d\n",a[i-1]);
+            printf("%d\n",a[i]);
             a[i-1]='\0';
             i--;
         }
